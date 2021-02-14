408a6c02d1c835cc8a966e68d6d78d3ae42e4c2d
user <user@puter.home.net>
2021-02-14 18:16:50 +0200

    Expose enums needed for attributes

--- orthogram/__init__.py
+++ orthogram/__init__.py
@@ -1,5 +1,11 @@
 __version__ = '0.4.3'
 
+from .attributes import (
+    LabelPosition,
+    Orientation,
+    Side,
+)
+
 from .build import Builder
 from .diagram import DiagramDef
 from .functions import load_ddf, translate, translate_dir, write_svg
